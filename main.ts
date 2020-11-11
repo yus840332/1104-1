946563a2e4bade653b99ab659588894ef796f7bf
yus840332 <70998708+yus840332@users.noreply.github.com>
2020-11-11 15:01:01 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,24 @@
-let i = 0
-while (i < 10) {
-    bitbot.rotatems(BBRobotDirection.Left, 30, 400)
-    basic.showNumber(i)
-    basic.showIcon(IconNames.Sad)
-    i += 1
-}
-basic.showIcon(IconNames.Happy)
+let 開始時間 = 0
 basic.forever(function () {
-	
+    if (bitbot.readLine(BBLineSensor.Left) == 0 && bitbot.readLine(BBLineSensor.Right) == 0) {
+        basic.showNumber(0)
+        bitbot.stop(BBStopMode.Brake)
+        bitbot.rotate(BBRobotDirection.Right, 40)
+        開始時間 = input.runningTime()
+        while (bitbot.readLine(BBLineSensor.Left) == 1 && bitbot.readLine(BBLineSensor.Right) == 1 && input.runningTime() - 開始時間 <= 400 || input.runningTime() - 開始時間 > 400) {
+            bitbot.rotate(BBRobotDirection.Right, 40)
+        }
+    }
+    if (bitbot.readLine(BBLineSensor.Left) == 1 && bitbot.readLine(BBLineSensor.Right) == 0) {
+        basic.showNumber(1)
+        bitbot.rotate(BBRobotDirection.Left, 16)
+    }
+    if (bitbot.readLine(BBLineSensor.Left) == 0 && bitbot.readLine(BBLineSensor.Right) == 1) {
+        basic.showNumber(2)
+        bitbot.rotate(BBRobotDirection.Right, 16)
+    }
+    if (bitbot.readLine(BBLineSensor.Left) == 1 && bitbot.readLine(BBLineSensor.Right) == 1) {
+        basic.showNumber(3)
+        bitbot.go(BBDirection.Forward, 60)
+    }
 })
